a9a6b8b9717a46bc0bdaff1ddc3caec290f71206
MAHIPAL SINGH <manish7.2.2004@gmail.com>
2025-12-19 00:04:07 +0530

    bulding userCard

---
+++ src/components/UserFeedCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const UserFeedCard = ({ user }) => {
+  const { firstName, lastName, age, gender, skills, photoUrl, about } = user;
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img src={photoUrl} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + " " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-between mx-3">
+          <button className="btn btn-secondary ">Ignore </button>
+          <button className="btn btn-primary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserFeedCard;
