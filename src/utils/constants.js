09f4b8ea5ae688041dbd07f69e460f0c9abe64f9
MAHIPAL SINGH <manish7.2.2004@gmail.com>
2025-12-24 23:04:11 +0530

    "added env file"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
-//production
-// export const BASE_URL = "/api";
-
-//localhost
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
